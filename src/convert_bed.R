9355077f3ec6cf9d0cf1441843f129fa2fefa64a
Kushal Kumar Dey <kkdey@uchicago.edu>
2015-08-01 20:17:00 -0500

    converted from bed to txt with imputation of missing genotypes

---
+++ src/convert_bed.R
@@ -0,0 +1,14 @@
+library(snpStats)
+reads <- read.plink(bed="../external_data/haak_fig3_set.bed",
+                    bim="../external_data/haak_fig3_set.bim",
+                    fam="../external_data/haak_fig3_set.fam")
+gm = as(reads$genotypes, 'numeric')
+
+gm_imputed <- apply(gm,2,function(x)
+           {
+              y=x;
+              y[is.na(x)]=rbinom(1,2,mean(x,na.rm=TRUE)/2);
+              return(y);
+            })
+
+write.table(gm_imputed, '../external_data/imputed_haak_fig3_genotypes.txt');
