2b2fd48d3a3293c3129f765376bacb460eca9786
Kushal K Dey <kshldey@gmail.com>
2015-09-12 18:46:06 -0500

    fixed the bug in the sampling correction code, saved the plots of files under sampling and non sampling correction

--- src/ancient_structure.R
+++ src/ancient_structure.R
@@ -1,5 +1,4 @@
 
-
 library(gtools)
 library(SQUAREM)
 library(parallel)
@@ -9,45 +8,47 @@ source('utilities.R')
 
 # The main function that fits the ancient structure and gives the final topic proportions and allele frequencies
 
-ancient_structure <- function(geno_data, f_known, K_unknown, max_iter, eps=1e-04, use_squarem=FALSE)
+ancient_structure <- function(geno_data, f_obs, n_obs, K_unknown, max_iter, eps=1e-04, use_squarem=FALSE)
 {
-  
-  f_known <- matrix(f_known, nrow=dim(geno_data)[2])
-  K_known = dim(f_known)[2]
+  f_obs <- matrix(f_obs, nrow=dim(geno_data)[2])
+  K_known = dim(f_obs)[2]
   K_pooled = K_known + K_unknown
   nsamp <- dim(geno_data)[1];
   nSNPs <- dim(geno_data)[2];
-  
+
   # determining initial values of f_unknown and q
-  
-  f_unknown_initial = matrix(nrow = nSNPs, ncol = K_unknown, runif(nSNPs*K_unknown))
+  f_unknown_initial = matrix(nrow = nSNPs, ncol = K_unknown, runif(nSNPs*K_unknown));
   q_initial = matrix(nrow = nsamp, ncol = K_pooled, rdirichlet(nsamp, rep(1/K_pooled,K_pooled)));
   rev_q_initial <-as.matrix(t(apply(q_initial, 1, function(x) reverse_transform(x))));
-  
+
   # trying to add some perturbation 'eps' to counter values of 0 and 1 in allele frequencies (eps user defined)
-  
-  ind0 <- which(f_known < eps, arr.ind=T);
-  ind1 <- which(f_known > (1-eps), arr.ind=T);
-  f_known[ind1] = 1-eps
-  f_known[ind0] = eps
-  
+
+  ind0 <- which(f_obs < eps, arr.ind=T);
+  ind1 <- which(f_obs > (1-eps), arr.ind=T);
+  f_obs[ind1] = 1-eps
+  f_obs[ind0] = eps
+
+  f_known_initial = f_obs;
+
   # transforming f_known and f_unknown to logit form to make it unconstrained, needed for squarem input
-  
-  logit_f_known <- logit(f_known);
+
+  logit_f_known_initial <- logit(f_known_initial);
   logit_f_unknown_initial <- logit(f_unknown_initial);
-  
+
   # pooling the transformed q and the logit transformed f_known and f_unknown
-  
-  param_vec_in <- c(as.vector(rev_q_initial),as.vector(logit_f_unknown_initial),as.vector(logit_f_known));
-  
+
+  param_vec_in <- c(as.vector(rev_q_initial),as.vector(logit_f_unknown_initial),as.vector(logit_f_known_initial));
+
   # using squarem
-  
+
   if(use_squarem)
   {
     res <- squarem(par=as.numeric(param_vec_in),
                    fixptfn=update_squarem,
                    objfn= loglik_squarem,
                    geno_data = geno_data,
+                   f_obs =f_obs,
+                   n_obs = n_obs,
                    nsamp = nsamp,
                    nSNPs = nSNPs,
                    K_unknown = K_unknown,
@@ -60,35 +61,42 @@ ancient_structure <- function(geno_data, f_known, K_unknown, max_iter, eps=1e-04
     f_unknown <- inv.logit(matrix(temp[0:(nSNPs*K_unknown)], nrow=nSNPs, ncol=K_unknown))
     beg = nSNPs*K_unknown + 1;
     end = nSNPs*K_pooled;
-    f_known <- inv.logit(matrix(temp[beg:end], nrow=nSNPs, ncol=K_known))
-    
+    if(beg >end)
+      f_known <- inv.logit(matrix(0, nrow=nSNPs, ncol=K_known));
+    if(end >beg)
+      f_known <- inv.logit(matrix(temp[beg:end], nrow=nSNPs, ncol=K_known));
+
+
     out <- list("q"=q, "f_known"=f_known,"f_unknown"=f_unknown);
-    
+
     return(out)
   }
-  
+
   if(!use_squarem)
   {
-    for(iter in 1:max_iter) { 
-      out = update_squarem(param_vec_in, geno_data, nsamp, K_unknown, K_known, nSNPs);
-      loglik <- loglik_squarem(param_vec_in, geno_data, nsamp, K_unknown, K_known, nSNPs);
+    for(iter in 1:max_iter) {
+      out = update_squarem(param_vec_in, geno_data, f_obs, n_obs, nsamp, K_unknown, K_known, nSNPs);
+      loglik <- loglik_squarem(param_vec_in, geno_data, f_obs, n_obs, nsamp, K_unknown, K_known, nSNPs);
       cat('Neg Loglikelihood at iteration',iter,':',loglik,'\n');
-      param_vec_in <- out; 
+      param_vec_in <- out;
       }
-    
+
       rev_q = matrix(param_vec_in[(1:(nsamp*(K_pooled-1)))],nrow = nsamp, ncol = (K_pooled-1));
       q <- t(apply(rev_q, 1,function(x) transform(x)));
       temp <- param_vec_in[-(1:(nsamp*(K_pooled-1)))];
       f_unknown <- inv.logit(matrix(temp[0:(nSNPs*K_unknown)], nrow=nSNPs, ncol=K_unknown))
       beg = nSNPs*K_unknown + 1;
       end = nSNPs*K_pooled;
-      f_known <- inv.logit(matrix(temp[beg:end], nrow=nSNPs, ncol=K_known))
-      
-      out <- list("q"=q, "f_known"=f_known,"f_unknown"=f_unknown);
-      
-      return(out)
-  }
-  
+      if(beg >end)
+        f_known <- inv.logit(matrix(0, nrow=nSNPs, ncol=K_known));
+      if(end >beg)
+        f_known <- inv.logit(matrix(temp[beg:end], nrow=nSNPs, ncol=K_known));
 
-  
+      out_list <- list("q"=q, "f_known"=f_known,"f_unknown"=f_unknown);
+
+      return(out_list)
+  }
+
+
+
 }
--- src/example_sim.R
+++ src/example_sim.R
@@ -1,9 +1,9 @@
 
 ### SQUAREM implementation (Main function)
-
+set.seed(10)
 source('simulate_model.R')
 source('ancient_structure.R');
-## Assignment operations 
+## Assignment operations
 npop=5;
 nsamp_per_pop=100;
 nclusters = 3
@@ -25,21 +25,47 @@ simulate_allele_freq <- function(alpha, nSNPs){
 
 # we're working with genotype data so equal number of variants across freq range
 alpha <- c(1,1,1,1);
-
+nSNPs <- 1000;
 # size = nSNPs x nclusters
-freq_mat <- matrix(unlist(lapply(1:nclusters, function(n) simulate_allele_freq(alpha[n],nSNPs = 1000))),ncol=nclusters);
+freq_mat <- matrix(unlist(lapply(1:nclusters, function(n) simulate_allele_freq(alpha[n],nSNPs = nSNPs))),ncol=nclusters);
 
 
 data <- simulate_binomial_model(omega,freq_mat)
 
 
 
-K_known = 2
+K_known = 2;
 K_unknown = 1;
 K_pooled <- K_known +K_unknown;
+
+f_obs <- matrix(0,nrow=nSNPs, ncol=K_known);
+n_obs = rep(2,K_known);
+if(min(n_obs)!=0){
+for (k in 1:K_known){
+  f_obs[,k] = rbinom(nSNPs,2*n_obs[k], freq_mat[,k])/(2*n_obs[k])
+}
+}
+
 system.time(
-out <- ancient_structure(geno_data = data, f_known = freq_mat[,(K_unknown+1):K_pooled], K_unknown = K_unknown, max_iter = 200)
+out <- ancient_structure(geno_data = data, f_obs = f_obs, n_obs = n_obs, K_unknown = K_unknown, max_iter = 600)
 )
 
 barplot(t(omega),col=2:(nclusters+1),axisnames=F,space=0,border=NA,main=paste("true structure: No. of clusters=",nclusters),las=1,ylim=c(0,1),cex.axis=1.5,cex.main=1.4)
-barplot(t(out$q),col=2:(nclusters+1),axisnames=F,space=0,border=NA,main=paste("estd structure: No. of clusters=",nclusters),las=1,ylim=c(0,1),cex.axis=1.5,cex.main=1.4)
+
+docweights <- out$q;
+
+library(permute);
+library("BioPhysConnectoR");
+perm_set=rbind(1:nclusters,allPerms(1:nclusters));
+diff=array(0,dim(perm_set)[1]);
+for (p in 1:dim(perm_set)[1])
+{
+  temp=docweights[,perm_set[p,]];
+  diff[p]=fnorm(temp,omega);
+}
+
+p_star=which(diff==min(diff));
+docweights=docweights[,perm_set[p_star,]];
+
+
+barplot(t(docweights),col=2:(nclusters+1),axisnames=F,space=0,border=NA,main=paste("estd structure: No. of clusters=",nclusters),las=1,ylim=c(0,1),cex.axis=1.5,cex.main=1.4)
